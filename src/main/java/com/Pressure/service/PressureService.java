8c1992c4ef2555af017f538517bbc0d211aae98f
Giansimone Coccia <S1097845@studenti.univpm.it>
2021-12-09 19:02:13 +0100

    Correzione pom.xml e aggiunta classe getJSONFromPman

--- src/main/java/com/Pressure/service/PressureService.java
+++ src/main/java/com/Pressure/service/PressureService.java
@@ -3,10 +3,38 @@ package com.Pressure.service;
 import org.json.simple.*;
 import com.Pressure.model.*;
 
+/**
+ * Public interface con metodi astratti basilari che verranno
+ * implementati da altre classi (analogia con CRUD)
+ * @author Giansimone&Walter
+ *
+ */
 public interface PressureService {
 	
+	/**
+	 * 
+	 * @param city The city
+	 * @return A JSONObject 
+	 */
 	public abstract JSONObject toJSON(City city);
+	
+	/**
+	 * 
+	 * @param city The city
+	 * @return The Postman's JSONObject
+	 */
 	public abstract JSONObject getJSONfromPman(String city);
+	
+	/**
+	 * 
+	 * @param pressure Is the JSONObject passed
+	 * @return The city
+	 */
 	public abstract City getPressure(JSONObject pressure);
+	
+	/**
+	 * 
+	 * @param obj The object to save
+	 */
 	public abstract void save(JSONObject obj);
 }
---
+++ src/main/java/com/Pressure/service/PressureServiceImpl.java
@@ -0,0 +1,61 @@
+package com.Pressure.service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.ParseException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.springframework.stereotype.Service;
+
+/**
+ * Questa classe implementa i metodi astratti di PressureService
+ * @author Giansimone&Walter
+ *
+ */
+@Service
+public class PressureServiceImpl implements PressureService{
+
+	//Stringa che contiene l'API key da utilizzare nella chiamata
+	private String apiKey = "10b2f29f8e21bd179b27ff96923bca4a";
+	//Stringa che contiene l'URL che verra richiamato
+	private String url = "api.openweathermap.org/data/2.5/weather?q=";
+
+	@Override
+	public JSONObject getJSONfromPman(String city) {
+		JSONObject description = null;
+
+		try {
+			URLConnection openConnection = new URL(url + city + "&appid=" + apiKey).openConnection();
+			InputStream input = openConnection.getInputStream();
+
+			String data = "";
+			String line = "";
+
+			try {
+				InputStreamReader reader = new InputStreamReader(input);
+				BufferedReader buffer = new BufferedReader(reader);
+
+				while((line = buffer.readLine()) != null) {
+					data += line;
+				}
+			} finally {
+				input.close();
+			}
+			description = (JSONObject) JSONValue.parseWithException(data);
+		
+		} catch(IOException IOe) {
+			IOe.printStackTrace();
+		}catch(ParseException parseE) {
+			parseE.printStackTrace();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return description;
+	}
+}
