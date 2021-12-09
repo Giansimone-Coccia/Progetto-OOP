8a13274497025c47ced390ecae10738fc009bac6
Giansimone Coccia <S1097845@studenti.univpm.it>
2021-12-09 16:58:47 +0100

    First commit

---
+++ src/main/java/com/Pressure/PressureChecker/PressureCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.Pressure.PressureChecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PressureCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PressureCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Pressure/PressureChecker/PressureCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pressure.PressureChecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PressureCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
