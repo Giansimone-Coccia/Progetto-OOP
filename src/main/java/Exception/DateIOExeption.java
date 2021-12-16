32731796f95bbe965b774640fe5f3161ec40ba91
Giansimone Coccia <S1097845@studenti.univpm.it>
2021-12-16 17:45:25 +0100

    Aggiunta commento

--- src/main/java/Exception/DateIOExeption.java
+++ src/main/java/Exception/DateIOExeption.java
@@ -1,5 +1,10 @@
 package Exception;
 
+/**
+ * Classe per l'eccezione personalizzata
+ * @author Giansimone&Walter
+ *
+ */
 public class DateIOExeption extends Exception{
 	
 	public DateIOExeption() {
