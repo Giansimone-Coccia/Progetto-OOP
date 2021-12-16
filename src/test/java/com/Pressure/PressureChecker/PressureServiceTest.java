a190aeedf8c27f42fc6c64e62ef3aa5c6b85c4f8
Giansimone Coccia <S1097845@studenti.univpm.it>
2021-12-16 16:34:02 +0100

    Modifica test

--- src/test/java/com/Pressure/PressureChecker/PressureServiceTest.java
+++ src/test/java/com/Pressure/PressureChecker/PressureServiceTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import com.Pressure.model.Pressure;
 import com.Pressure.service.PressureServiceImpl;
 
 class PressureServiceTest {
