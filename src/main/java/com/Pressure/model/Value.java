782dc9f6ff3d2149d720ed4ece336a751f38f69e
Giansimone Coccia <S1097845@studenti.univpm.it>
2021-12-21 19:36:20 +0100

    Aggiunta commenti per javadoc

--- src/main/java/com/Pressure/model/Value.java
+++ src/main/java/com/Pressure/model/Value.java
@@ -1,10 +1,39 @@
 package com.Pressure.model;
 
+/**
+ * Interfaccia che viene implementata dalla classe Pressure
+ * @author Giansimone&Walter
+ *
+ */
 public interface Value {
 	
+	/**
+	 * This method is used to set the value passed as a parameter
+	 * @param value The value to set
+	 */
 	public void setValue(long value);
+	
+	/**
+	 * 
+	 * @return The maximum value
+	 */
 	public long getValue_max();
+	
+	/**
+	 * 
+	 * @return The minimum value
+	 */
 	public long getValue_min();
+	
+	/**
+	 * 
+	 * @return The medium value
+	 */
 	public double getValue_med();
+	
+	/**
+	 * 
+	 * @return The difference between two values
+	 */
 	public long getValue_diff();
 }
