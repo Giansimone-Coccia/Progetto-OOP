d48849de016bb21b90ea98a45be1b62468323e46
Giansimone Coccia <S1097845@studenti.univpm.it>
2021-12-22 19:50:56 +0100

    Ultimo commit

--- src/main/java/com/Pressure/PressureChecker/PressureCheckerApplication.java
+++ src/main/java/com/Pressure/PressureChecker/PressureCheckerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages= {"com.Pressure.controller", "com.Pressure.PressureChecker", "com.Pressure.service", "com.Pressure.model", "com.Pressure.exceptions","com.Pressure.utilities"})
 public class PressureCheckerApplication {
-
+ 
 	public static void main(String[] args) {
 		SpringApplication.run(PressureCheckerApplication.class, args);
 	}
